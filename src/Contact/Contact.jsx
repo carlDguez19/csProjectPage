5af97aabac596921187c22f398def71c9887ced4
Carlos <110745188+carlDguez19@users.noreply.github.com>
2026-03-27 13:06:38 -0600

    added icons to contact pag. then will pull for new contact me message

--- src/Contact/Contact.jsx
+++ src/Contact/Contact.jsx
@@ -1,34 +1,43 @@
+import email from '../assets/icons/email.svg'
+import linkedIn from '../assets/icons/linkedin.svg'
+import github from '../assets/icons/githubIcon.svg'
+import styles from './Contact.module.css'
+
+
 export function Contact() {
   return (
-    <section className="contactSection">
+    <section className={styles.contactSection}>
+      <div className={styles.contactWrapper}>
+        <h2 className={styles.contactTitle}>Get in Touch</h2>
 
-      <h2 className="contactTitle">Get in Touch</h2>
+        <p className="contactIntro">
+          The best way to reach me is through email or any of the links below.
+        </p>
 
-      <p className="contactIntro">
-        The best way to reach me is through email or any of the links below.
-      </p>
+        <ul className="contactList">
+          <li>
+            <a href="mailto:carlosdguezwcs@gmail.com" target="_blank" rel="noopener noreferrer">
+              <img src={email} alt="email icon" />
+              Email
+            </a>
+          </li>
 
-      <ul className="contactList">
-        <li>
-          <a href="mailto:carlosdguezwcs@gmail.com" target="_blank" rel="noopener noreferrer">
-            Email
-          </a>
-        </li>
+          <li>
+            <a href="https://www.linkedin.com/in/carlosdominguezdev/" target="_blank" rel="noopener noreferrer">
+              <img src={linkedIn} alt="LinkedIn icon" />
+              LinkedIn
+            </a>
+          </li>
 
-        <li>
-          <a href="https://www.linkedin.com/in/carlosdominguezdev/" target="_blank" rel="noopener noreferrer">
-            LinkedIn
-          </a>
-        </li>
+          <li>
+            <a href="https://github.com/carlDguez19" target="_blank" rel="noopener noreferrer">
+              <img src={github} alt="GitHub Icon" />
+              GitHub
+            </a>
+          </li>
 
-        <li>
-          <a href="https://github.com/carlDguez19" target="_blank" rel="noopener noreferrer">
-            GitHub
-          </a>
-        </li>
-
-      </ul>
-
+        </ul>
+      </div>
     </section>
   );
 }
