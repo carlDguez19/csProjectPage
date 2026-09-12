e84d15828ee375b1edab66065e36bb0939ecb4aa
Carlos <110745188+carlDguez19@users.noreply.github.com>
2026-09-12 14:33:23 -0600

    added inventory app and rearranged my side and featured projects

--- src/Project/Project.jsx
+++ src/Project/Project.jsx
@@ -11,34 +11,39 @@ import todoListTh from '../assets/thumbnails/todolistTh.png'
 import libraryTh from '../assets/thumbnails/libraryTh.png'
 import calculatorTh from '../assets/thumbnails/calculatorTh.png'
 import etchTh from '../assets/thumbnails/etchTh.png'
+import invTh from '../assets/thumbnails/inventoryTh.png'
 import styles from './Project.module.css'
 
 export function Project(){
-    const battleshipDesc = "A fully interactive Batleship game built with modular css," +
-    " responsive layout techniques, and a clean game-state logic. Designed for clarity," +
-    " smooth gameplay, and a polished user experience";
+    const memoryGameDesc = "A dynamic React memory game powered by the PokeAPI, featuring real-time score tracking," +
+    " best-score persistence, and fully randomized card layouts. Built with hooks for state management, asynchronous data" +
+    " fetching, and responsive UI updates, the game challenges players to select unique Pokemon without repeating choices." +
+    " Designed with modular components, clean logic, and a polished interface, this project demonstrates practical React patterns" +
+    " and API-driven interactivity."
 
     const reactShoppingDesc = "A responsive React application featuring dynamic product listings," +
     " real-time cart updates, and a clean, intuitive sshopping experiece. Users can browse items," +
     " adjust quantities, and view a live cart summary powered by component-based management." +
     " Built with modular architecture and reusable UI components for maintainability and scalability."
 
-    const sqlzooDesc = "A structured collection of SQL solutions built while completing the SQLZoo" +
-    " exercises, rewritten into clean, well-organized .sql files. Each section includes questions," +
-    " and formatted queries to demonstrate understanding of joins, subqueries, grouping, and relational logic." +
-    " This repo serves as both a mastery checkpoint and a reusable referance for future SQl work."
+    const inventoryProjDesc = "A full-stack inventory management application built with Express, modular routing," +
+    " and a structured CRUD architecture. Features include dynamic item creation, category assignment, real-time" +
+    " updates, and clean form handling powered by server-side validation. Designed for clarity, maintainability," +
+    " and a smooth user workflow, this project demonstrates practical backend logic, organized controllers," +
+    " and a scalable data model suitable for real-world inventory systems."
 
     return (
         <section className= {styles.projectsSec}>
             <div className={`${styles.projectsSection} ${styles.fadeIn}`}>
                 <h2 className={styles.projectHeader}>Featured Projects</h2>
                 {/* each card will contain the following: title, description, stack, thnail, thAlt, demoLink, githubLink, demoIcon, githubIcon */}
-                <ProjectCard title="Battleship" description={battleshipDesc} stack="Javascript, CSS, HTML, Netlify" thnail={battleshipTh} thAlt="Battleship Thumbnail" demoLink="https://battleship-carlos.netlify.app/" githubLink="https://github.com/carlDguez19/odinBattleship2" demoIcon={demoIcon} githubIcon={githubIcon} githubAlt="Github link" demoAlt="Demo link"/>
+                <ProjectCard title="Inventory Management" description={inventoryProjDesc} stack="Express, Node.js, JavaScript, EJS Templates, CSS, PostgreSql, MVC Architecture, Railway" thnail={invTh} thAlt="Inventory project Thumbnail" demoLink="https://odininventoryexpress-production.up.railway.app/" githubLink="https://github.com/carlDguez19/odinInventoryExpress" demoIcon={demoIcon} githubIcon={githubIcon} githubAlt="Github link" demoAlt="Demo link" />
                 <ProjectCard title="React Shopping Cart" description={reactShoppingDesc} stack="React, Javascript, CSS Modules, React Router, Netlify" thnail={reactShopTh} thAlt="React Shop Thumbnail" demoLink="https://carlosreactshp.netlify.app" githubLink="https://github.com/carlDguez19/odinShoppingCart" demoIcon={demoIcon} githubIcon={githubIcon} githubAlt="Github link" demoAlt="Demo link"/>
-                <ProjectCard title="SQLZoo Mastery Repository" description={sqlzooDesc} stack="SQL, PostgreSQL(syntax-compatible), SQLZoo problem sets" thnail={sqlzooTh} thAlt="sqlzoo repo Thumbnail" githubLink="https://github.com/carlDguez19/odinSqlPractice" githubIcon={githubIcon} githubAlt="Github link"/>
+                <ProjectCard title="Memory Game" description={memoryGameDesc} stack="React, Javascript, CSS, Vite, PokeAPI" thnail={pokeMemGame} thAlt="Pokemon Memory Game Thumbnail" demoLink="https://memorygamecarlos.netlify.app/" githubLink="https://github.com/carlDguez19/odinMemoryGame" demoIcon={demoIcon} githubIcon={githubIcon} githubAlt="Github link" demoAlt="Demo link" />
                 <h2 className={styles.projectHeader}>Side Projects</h2>
                 <div className={styles.sideProjGrid}>
-                    <ProjectCard small title="Memory Game" stack="React, Javascript, CSS, Vite, PokeAPI" thnail={pokeMemGame} thAlt="Pokemon Memory Game Thumbnail" demoLink="https://memorygamecarlos.netlify.app/" githubLink="https://github.com/carlDguez19/odinMemoryGame" demoIcon={demoIcon} githubIcon={githubIcon} githubAlt="Github link" demoAlt="Demo link"/>
+                    <ProjectCard small title="Battleship" stack="Javascript, CSS, HTML, Netlify" thnail={battleshipTh} thAlt="Battleship Thumbnail" demoLink="https://battleship-carlos.netlify.app/" githubLink="https://github.com/carlDguez19/odinBattleship2" demoIcon={demoIcon} githubIcon={githubIcon} githubAlt="Github link" demoAlt="Demo link"/>
+                    <ProjectCard small title="SQLZoo Mastery Repository" stack="SQL, PostgreSQL(syntax-compatible), SQLZoo problem sets" thnail={sqlzooTh} thAlt="sqlzoo repo Thumbnail" githubLink="https://github.com/carlDguez19/odinSqlPractice" githubIcon={githubIcon} githubAlt="Github link"/>
                     <ProjectCard small title="CV App" stack="React, Javascript, CSS, Vite" thnail={cvappTh} thAlt="cvApp Thumbnail" demoLink="https://cvapp-carlos.netlify.app/" githubLink="https://github.com/carlDguez19/odinCvapp" demoIcon={demoIcon} githubIcon={githubIcon} githubAlt="Github link" demoAlt="Demo link"/>
                     <ProjectCard small title="Climate App" stack="Javascript, Webpack, CSS, HTML5, WeatherAPI" thnail={climateApp} thAlt="Climate app Thumbnail" demoLink="https://climateapp-carlos.netlify.app/" githubLink="https://github.com/carlDguez19/odinClimate" demoIcon={demoIcon} githubIcon={githubIcon} githubAlt="Github link" demoAlt="Demo link"/>
                     <ProjectCard small title="To-Do List" stack="Javascript, CSS, HTML5, Webpack, date-fns, localStorage" thnail={todoListTh} thAlt="To Do List Thumbnail" demoLink="https://to-dolist-carlos.netlify.app/" githubLink="https://github.com/carlDguez19/odinToDoList" demoIcon={demoIcon} githubIcon={githubIcon} githubAlt="Github link" demoAlt="Demo link"/>
